8e21e29bb4c1f754d87dc764a2bbd4dc21bcab76
vkv0220 <36685410+vkv0220@users.noreply.github.com>
2018-11-20 16:56:38 +0700

    Add scripts

---
+++ export.ps1
@@ -0,0 +1,28 @@
+﻿# Подгружаем набор командлетов
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+Import-Module ActiveDirectory
+# Описываем массив для данных
+$ListContacts = @();
+# выгружаем список почтовых ящиков из конкретной OU или всего домена (можно также указать дополнительные фильтры при необходимости)
+$ListEmplMailbox = Get-Mailbox #|-OrganizationalUnit "domain.name/OE/Contacts"
+# Формируем массив ящиков с необходимыми полями
+Foreach ($mailbox in $ListEmplMailbox ){
+	# Запрашиваем атрибуты из учетки в AD
+	$User = $mailbox.SamAccountName | Get-ADUser -Properties *
+	$Contact = new-object -TypeName PSObject
+	#ФИО
+	$Contact | Add-Member -MemberType NoteProperty -Name FIO -Value $User.DisplayName
+	#Alias как правило совпадает с SamAccountName
+	$Contact | Add-Member -MemberType NoteProperty -Name Alias -Value $mailbox.Alias
+	#email
+	$Contact | Add-Member -MemberType NoteProperty -Name Email -Value $User.EmailAddress
+	#должность
+	$Contact | Add-Member -MemberType NoteProperty -Name Title -Value $User.Title
+	#Телефон
+	$Contact | Add-Member -MemberType NoteProperty -Name Phone -Value $User.telephoneNumber
+	#Компания
+	$Contact | Add-Member -MemberType NoteProperty -Name Company -Value "CompanyName"
+	$ListContacts += $Contact
+}
+# Выгружаем массив в файл
+$ListContacts | Export-Csv -Path "C:\Temp\ListContact_domain2.csv" -Encoding UTF8 -NoTypeInformation -Delimiter ";"
---
+++ import.ps1
@@ -0,0 +1,35 @@
+﻿# Подгружаем набор командлетов
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010
+# Указываем файл из которого брать информацию о контактах
+Set-Location "C:\Obmen"
+$ListContacts = Import-Csv .\ListContact_domain2.csv -Delimiter ";" #| select -First 40
+# Загружаем имеющиеся в AD контакты
+$ListExistContacts = Get-Contact
+# начинаем создание контактов
+foreach ($Contact in $ListContacts)
+{
+	$Contact_Exist = $null
+	# Проверяем существует ли такой контакт
+	foreach ($ExistContact in $ListExistContacts)
+	{
+		if ($Contact.Mail -eq $ExistContact.WindowsEmailAddress) {$Contact_Exist = $true }
+	}
+	if ($Contact_Exist) {"Контакт существует: " + $Contact.Name }
+	else
+	{
+		# Если такого контакта нет, то создаем его
+		# парсим строку с ФИО на массив Фамилия Имя Отчетство
+		# $fio[0] - Фамилия
+		# $fio[1] - Имя
+		# $fio[2] - Отчество
+		$fio = $Contact.FIO -split " "
+		$io = $fio[1] + " " + $fio[2]
+		# Дописываем префикс, чтобы не попасть в имеющиеся алиасы
+		$MailNickName = $Contact.Alias + "_contact"
+		# Создаем контакт
+		$newcontact = New-MailContact -Name $Contact.FIO -DisplayName $Contact.FIO -LastName $io -FirstName $fio[0] -Alias $MailNickName -OrganizationalUnit "domain.name/OE/Contacts" -ExternalEmailAddress $Contact.Email
+		# Дописываем в созданном контакте компанию должность и телефон
+		Set-Contact -Identity $newcontact.Identity -Company $Contact.Company -Title $Contact.Title -Phone $Contact.Phone
+		$newcontact.Identity
+	}
+}
